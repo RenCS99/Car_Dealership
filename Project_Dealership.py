8ae174ea2cb9cf0a0144a2ba0b98eda45eaecc18
RenCS99 <65922620+RenCS99@users.noreply.github.com>
2020-11-23 21:15:59 -0800

    Updated the program's framework

---
+++ Project_Dealership.py
@@ -0,0 +1,49 @@
+import sqlite3
+from sqlite3 import Error
+
+def openConnection(_dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
+
+def closeConnection(_conn, _dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Close database: ", _dbFile)
+
+    try:
+        _conn.close()
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def startOptions(_conn, _dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Preparing the program")
+
+    
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def main():
+    database = r"data/Dealership.db"
+
+    # create a database connection
+    conn = openConnection(database)
+    with conn:
+        startOptions(conn)
+
+
+if __name__ == '__main__':
+    main()
